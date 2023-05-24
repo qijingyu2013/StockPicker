741aec4b643d935b4a88647d833fb8225080c04f
jingyu qi <qijingyu2009@gmail.com>
2023-05-24 20:04:04 +0800

    修改更新流程

--- RobotDaily.py
+++ RobotDaily.py
@@ -9,19 +9,20 @@ from utils import currentTime, TOKEN
 
 def main():
     print('###初始化执行任务')
-    print('[1] 获取满足放量的票')
-    print('[2] 更新股票行情')
+    print('[1] 更新股票行情')
+    print('[2] 更新股票信息')
     print('[3] 获取日连阴票')
     print('[4] 获取满足倍量的票')
-    print('[5] 更新股票信息')
+    print('[5] 获取满足放量的票')
+    print('[6] 获取底部的票')
     ball.set_token(TOKEN)
     timestamp = currentTime()
     upgradeStockTrade(timestamp, 'daily')
+    upgradeStockList(timestamp)
     nineDailyData()
     fetchMultiplier()
     fetchReleaseVolume()
     fetchBottom()
-    upgradeStockList(timestamp)
 
-    
+
 main()
